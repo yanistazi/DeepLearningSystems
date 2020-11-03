66f1301f86845630c1830258b2971ba6126dfbc7
Tazi <taziy@lski2678.lan>
2020-11-03 17:22:29 -0500

    homework 3

---
+++ HW3/PB2_Yanis_Tazi - Jupyter Notebook_files/main.js
@@ -0,0 +1,68 @@
+define(['jquery', 'base/js/utils'], function ($, utils) {
+    function setupDOM() {
+        $('#maintoolbar-container').append(
+            $('<div>').attr('id', 'nbresuse-display')
+                      .addClass('btn-group')
+                      .addClass('pull-right')
+            .append(
+                $('<strong>').text('Memory: ')
+            ).append(
+                $('<span>').attr('id', 'nbresuse-mem')
+                           .attr('title', 'Actively used Memory (updates every 5s)')
+            )
+        );
+        // FIXME: Do something cleaner to get styles in here?
+        $('head').append(
+            $('<style>').html('.nbresuse-warn { background-color: #FFD2D2; color: #D8000C; }')
+        );
+        $('head').append(
+            $('<style>').html('#nbresuse-display { padding: 2px 8px; }')
+        );
+    }
+
+    var displayMetrics = function() {
+        if (document.hidden) {
+            // Don't poll when nobody is looking
+            return;
+        }
+        $.getJSON(utils.get_body_data('baseUrl') + 'metrics', function(data) {
+            // FIXME: Proper setups for MB and GB. MB should have 0 things
+            // after the ., but GB should have 2.
+            var display = Math.round(data['rss'] / (1024 * 1024));
+
+            var limits = data['limits'];
+            if ('memory' in limits) {
+                if ('rss' in limits['memory']) {
+                    display += " / " + Math.round(limits['memory']['rss'] / (1024 * 1024));
+                }
+                if (limits['memory']['warn']) {
+                    $('#nbresuse-display').addClass('nbresuse-warn');
+                } else {
+                    $('#nbresuse-display').removeClass('nbresuse-warn');
+                }
+            }
+            if (data['limits']['memory'] !== null) {
+            }
+            $('#nbresuse-mem').text(display + ' MB');
+        });
+    }
+
+    var load_ipython_extension = function () {
+        setupDOM();
+        displayMetrics();
+        // Update every five seconds, eh?
+        setInterval(displayMetrics, 1000 * 5);
+
+        document.addEventListener("visibilitychange", function() {
+            // Update instantly when user activates notebook tab
+            // FIXME: Turn off update timer completely when tab not in focus
+            if (!document.hidden) {
+                displayMetrics();
+            }
+        }, false);
+    };
+
+    return {
+        load_ipython_extension: load_ipython_extension,
+    };
+});
